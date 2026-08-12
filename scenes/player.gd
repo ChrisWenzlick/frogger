380ff6fea631f0ff2eb8eed53d21894245b733f4
ChrisWenzlick <23247122+ChrisWenzlick@users.noreply.github.com>
2026-08-12 14:01:19 -0500

    Add tree and finish player movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,9 +1,22 @@
-extends Node2D
+extends CharacterBody2D
 
 var direction: Vector2 = Vector2(1,0)
-var speed: int = 5
+var speed: int = 200
 
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down");
-	
-	position += direction * speed
+	velocity = direction * speed
+	animate()
+	move_and_slide()
+
+func animate():
+	if direction:
+		if abs(direction.x) > abs(direction.y):
+			$AnimatedSprite2D.flip_h = direction.x > 0
+			$AnimatedSprite2D.play("left")
+		elif direction.y < 0:
+			$AnimatedSprite2D.play("up")
+		else:
+			$AnimatedSprite2D.play("down")
+	else:
+		$AnimatedSprite2D.frame = 0
